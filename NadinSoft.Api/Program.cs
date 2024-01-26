8ed0050382fceaa0ea09b474c2e667230e9d3b10
mohsen bagheri <mohsenbgi1380@gmail.com>
2024-01-26 18:28:12 +0330

    Added ef and sql server connection

--- NadinSoft.Api/Program.cs
+++ NadinSoft.Api/Program.cs
@@ -1,11 +1,25 @@
+using NadinSoft.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddControllers();
+
+// setting dbContexts
+builder.Services.AddDatabaseConfiguration(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// init db
+app.InitDatabase();
 
 if (app.Environment.IsDevelopment())
 {
---
+++ NadinSoft.Infra.Data/NadinSoftContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NadinSoft.Infra.Data
+{
+    public class NadinSoftContext : DbContext
+    {
+        public NadinSoftContext(DbContextOptions<NadinSoftContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(NadinSoftContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
